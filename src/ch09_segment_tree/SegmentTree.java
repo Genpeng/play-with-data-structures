7c7bcf6ff8cd4b4b48bb3fe359b3e9e057d3b671
“Genpeng” <xgp1227@gmai.com>
2018-11-18 15:13:20 +0800

    [Modify] SegmentTree class

--- src/ch09_segment_tree/SegmentTree.java
+++ src/ch09_segment_tree/SegmentTree.java
@@ -25,6 +25,7 @@ public class SegmentTree<E> {
      * Constructs a full binary tree according to the specified array.
      *
      * @param arr E[], the specified array to construct a full binary tree
+     * @param merger Merger, a merger to provide a merge function
      */
     public SegmentTree(E[] arr, Merger<E> merger) {
         this.merger = merger;
@@ -68,11 +69,31 @@ public class SegmentTree<E> {
         return data.length;
     }
 
+    /**
+     * Returns the corresponding element according to the index.
+     *
+     * @param index int, the index of the element in the array
+     * @return E, the corresponding element at the given index
+     */
     public E get(int index) {
         if (index < 0 || index > data.length - 1) {
             throw new IllegalArgumentException("[ERROR] Illegal index!");
         }
         return data[index];
+    }
+
+    /**
+     * Returns the result in the query interval.
+     *
+     * @param queryL int, the lower bound index of query interval
+     * @param queryR int, the upper bound index of query interval
+     * @return E, the result of query
+     */
+    public E query(int queryL, int queryR) {
+        if (isIllegal(queryL) || isIllegal(queryR) || queryL > queryR) {
+            throw new IllegalArgumentException("[ERROR] Illegal query index!");
+        }
+        return query(0, 0, data.length - 1, queryL, queryR);
     }
 
     // ============================================================================ //
@@ -99,30 +120,84 @@ public class SegmentTree<E> {
     }
 
     /**
-     * Builds a segment tree with the index of its root is <code>rootIndex</code>, where
-     * the lower bound index of the interval is <code>lowerBoundIndex</code>, and the upper
-     * bound index of the interval is <code>upperBoundIndex</code>.
+     * Builds a segment tree with the index of its root is <code>treeIndex</code>, where
+     * the lower bound index of the interval is <code>l</code>, and the upper bound index
+     * of the interval is <code>r</code>.
      *
-     * @param rootIndex int, the index of the root of the segment tree in the static array
-     * @param lowerBoundIndex int, the index of the lower bound in the interval
-     * @param upperBoundIndex int, the index of the upper bound in the interval
+     * @param treeIndex int, the index of the root of the segment tree in the static array
+     * @param l         int, the index of the lower bound in the interval
+     * @param r         int, the index of the upper bound in the interval
      */
-    private void buildSegmentTree(int rootIndex, int lowerBoundIndex, int upperBoundIndex) {
-        if (lowerBoundIndex == upperBoundIndex) {
-            tree[rootIndex] = data[lowerBoundIndex];
+    private void buildSegmentTree(int treeIndex, int l, int r) {
+        if (l == r) {
+            tree[treeIndex] = data[l];
             return;
         }
 
         // calculate left and right child index
-        int leftChildIndex = leftChild(rootIndex);
-        int rightChildIndex = rightChild(rootIndex);
+        int leftChildIndex = leftChild(treeIndex);
+        int rightChildIndex = rightChild(treeIndex);
 
-        // int midIndex = (lowerBoundIndex + upperBoundIndex) / 2;
-        int midIndex = lowerBoundIndex + (upperBoundIndex - lowerBoundIndex) / 2;
-        buildSegmentTree(leftChildIndex, lowerBoundIndex, midIndex);
-        buildSegmentTree(rightChildIndex, midIndex + 1, upperBoundIndex);
+        // int midIndex = (l + r) / 2;
+        int midIndex = l + (r - l) / 2;
+        buildSegmentTree(leftChildIndex, l, midIndex);
+        buildSegmentTree(rightChildIndex, midIndex + 1, r);
 
-        tree[rootIndex] = merger.merge(tree[leftChildIndex], tree[rightChildIndex]);
+        tree[treeIndex] = merger.merge(tree[leftChildIndex], tree[rightChildIndex]);
+    }
+
+    /**
+     * Checks whether the given index is legal.
+     *
+     * @param index int, the position index to check
+     * @return boolean, true if the index is legal, and false otherwise
+     */
+    private boolean isLegal(int index) {
+        return index >= 0 && index <= data.length - 1;
+    }
+
+    /**
+     * Checks whether the given index is illegal.
+     *
+     * @param index int, the position index to check
+     * @return boolean, true if the index is illegal, and false otherwise
+     */
+    private boolean isIllegal(int index) {
+        return !isLegal(index);
+    }
+
+    /**
+     * Returns the result in the query interval, where the index of the root of the
+     * segment tree is <code>treeIndex</code>, the lower bound index of the root is
+     * <code>l</code>, the upper bound index of the root is <code>r</code>, the lower
+     * bound index of query interval is <code>queryL</code>, and the upper bound index
+     * of query interval is <code>queryR</code>.
+     *
+     * @param treeIndex int, the index of the root of the segment tree
+     * @param l         int, the lower bound index of the root
+     * @param r         int, the upper bound index of the root
+     * @param queryL    int, the lower bound index of query interval
+     * @param queryR    int, the upper bound index of query interval
+     * @return E, the result of query
+     */
+    private E query(int treeIndex, int l, int r, int queryL, int queryR) {
+        if (l == queryL && r == queryR) {
+            return tree[treeIndex];
+        }
+
+        int mid = l + (r - l) / 2;
+
+        int leftChildIndex = leftChild(treeIndex);
+        int rightChildIndex = rightChild(treeIndex);
+        if (queryR <= mid) {
+            return query(leftChildIndex, l, mid, queryL, queryR);
+        } else if (queryL > mid) {
+            return query(rightChildIndex, mid + 1, r, queryL, queryR);
+        } else {
+            E leftResult = query(leftChildIndex, l, mid, queryL, mid);
+            E rightResult = query(rightChildIndex, mid + 1, r, mid + 1, queryR);
+            return merger.merge(leftResult, rightResult);
+        }
     }
 
     // ============================================================================ //
--- src/ch09_segment_tree/TestSegmentTree.java
+++ src/ch09_segment_tree/TestSegmentTree.java
@@ -8,8 +8,14 @@ package ch09_segment_tree;
  */
 public class TestSegmentTree {
     public static void main(String[] args) {
+        // 测试线段树的构建
         Integer[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
         SegmentTree<Integer> segmentTree = new SegmentTree<>(nums, (a, b) -> a + b);
         System.out.println(segmentTree);
+
+        // 测试线段树的查询
+        System.out.println(segmentTree.query(0, 7));
+        System.out.println(segmentTree.query(0, 3));
+        System.out.println(segmentTree.query(0, 4));
     }
 }
