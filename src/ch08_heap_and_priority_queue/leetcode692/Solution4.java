669d3e9c04a4518c46a9436eccc68ab94dc4cf72
Genpeng <xgp1227@gmail.com>
2018-12-06 09:44:14 +0800

    [Modify] The solution4 of no. 692 problem in the LeetCode

--- src/ch08_heap_and_priority_queue/leetcode692/Solution4.java
+++ src/ch08_heap_and_priority_queue/leetcode692/Solution4.java
@@ -10,17 +10,17 @@ import java.util.*;
  * @author  StrongXGP (xgp1227@gmail.com)
  * @date    2018/12/05
  */
-public class Solution3 {
+public class Solution4 {
     public List<String> topKFrequent(String[] words, int k) {
         // 1. 统计单词的词频
-        // 时间复杂度为 O(n)
+        //    时间复杂度为 O(n)
         Map<String, Integer> wordFreq = new HashMap<>(16);
         for (String word : words) {
             wordFreq.put(word, wordFreq.getOrDefault(word, 0) + 1);
         }
 
         // 2. 使用桶排序将单词按照词频进行分组
-        // 时间复杂度为 O(n)
+        //    时间复杂度为 O(n)
         List<String>[] bucket = new List[words.length + 1];
         for (String word : wordFreq.keySet()) {
             int freq = wordFreq.get(word);
@@ -40,6 +40,6 @@ public class Solution3 {
                 result.addAll(bucket[i]);
             }
         }
-        return result;
+        return result.subList(0, k);
     }
 }
